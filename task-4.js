8042334432e803628e4de220d1e56b9eed5a86d8
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-28 00:58:43 +0600

    all task done

---
+++ task-4.js
@@ -0,0 +1,9 @@
+// 4. Checking if it's an Array
+// Instructions:
+// Create different variables, each containing either an array or a non-array value.
+var dataOne = "I'm not an array";
+var dataTwo = ["Dhaka", "barishal"];
+// Now use isArray to check if each variable is an array.
+console.log("Is Data One is an array: " + Array.isArray(dataOne));
+console.log("Is Data Two is an array: " + Array.isArray(dataTwo));
+// Print a message to the console indicating whether each variable is an array or not.
---
+++ task-5.js
@@ -0,0 +1,10 @@
+// 5. Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+let studentData = ["Nayeem", "Zioun"];
+let newStudentData = ["Rohan", "Sohan"];
+// Use the concat method to combine the two arrays into a new array.
+let totalStudents = studentData.concat(newStudentData);
+// Print both the original arrays and the combined array using console.log().
+console.log(totalStudents);
