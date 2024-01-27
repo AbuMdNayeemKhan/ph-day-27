d601cd28275e74c40866dd69b52e3c8811d4d19b
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-27 23:51:39 +0600

    learning more about array

--- array.js
+++ array.js
@@ -1,8 +1,27 @@
 let mydata = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 //get elements by elements
-console.log(mydata[3]);
+// console.log(mydata[3]);
 
 //change data by using index
 mydata[1] = 22;
-console.log(mydata);
+// console.log(mydata);
+
+//push a single data on that array
+mydata.push(11);
+// console.log(mydata);
+
+//pup a data
+mydata.pop();
+// console.log(mydata);
+
+//shift unshift
+mydata.shift();
+// console.log(mydata);
+
+//includes
+mydata.includes(22);
+// console.log(mydata.includes(22));
+
+//indexOf
+console.log(mydata.indexOf(22));
