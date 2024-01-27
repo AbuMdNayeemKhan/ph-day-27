30ca3eb3ddc5a600193cddebbc0a8a6d9bf75d06
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-01-28 00:40:06 +0600

    task two and task three done

---
+++ task-2.js
@@ -0,0 +1,12 @@
+// 2. Add or remove elements
+// Declare an array of 3 tourist destinations
+let destinations = ['dhaka', 'coxs bazar', 'sylet'];
+// Add a new tourist destination to your tourist array
+destinations.push('bandarboan');
+// Add two more to your array
+destinations.push('kuakata');
+destinations.push('rajsahi');
+// Remove the last tourist destination you have added
+destinations.pop();
+// display the final array as output
+console.log(destinations);
---
+++ task-3.js
@@ -0,0 +1,8 @@
+// 3. Checking Array Membership with ‘includes’
+// Instructions:
+// Create an array of books containing different book.
+let books = ['html book', 'css book', 'javascript book', 'react book', 'firebase book'];
+// Use the includes method to check if the array contains a javascript book.
+let checkBook = books.includes('javascript book');
+// Print a message to the console indicating whether the element is present in the array or not.
+console.log(checkBook);
